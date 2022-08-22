9defd22bfe88b36927aeb00421853a7a4dd601ab
cyberxploit <cyberxploit166@gmail.com>
2022-08-22 23:00:54 +0100

    First Commit

---
+++ ATM.py
@@ -0,0 +1,100 @@
+
+import os, sys, time, logging
+from datetime import datetime
+
+
+### To Do List ###
+
+# Insert your card
+# Authentication (PIN verification)
+# Choose from Menus (Check Balance, Transfer, Withdraw)
+# Repeat if yes else exit
+
+
+# Clear screen
+os.system('cls' if os.name == 'nt' else 'clear')
+
+
+# Current day and Time
+DT = datetime.now()
+print(DT)
+time.sleep(0.6)
+print('Please Slot in Your Card...')
+time.sleep(2)
+print('Loading Card...')
+time.sleep(1)
+print("""
+============================================
+======= Welcome to Cyber ATM center ========
+======= Author : Yazeed Ahmed   ============
+======= Version : 1.0             ==========
+============================================
+""")
+time.sleep(2)
+
+
+def transactions():
+     def check_balance():
+          pass
+     def withdraw():
+          
+          pass
+     def transfer():
+          pass
+     print("""
+What would you Like to Perform?
+1. Check Balance
+2. Withdraw
+3. Transfer
+4. Exit
+""")
+     transaction = int(input("Choose 1/2/3/4: "))
+     time.sleep(1)
+     if transaction == 1:
+          time.sleep(1)
+          check_balance()
+     elif transaction == 2:
+          time.sleep(1)
+          withdraw()
+     elif transaction == 3:
+          time.sleep(1)
+          transfer()
+     else:
+          print('GoodBye..')
+          sys.exit()
+
+
+def get_pin():
+     #for j in range(0,4):
+          #pin = int(input("Enter you PIN: "))
+          #with open('pins.txt', 'r') as p:
+               #check_pin = p.read()
+     pins = [1234, 1122, 1133, 1803, 1672, 1110, 1111]
+     for i in pins:
+          try:
+               for j in range(1,4):
+                    try:
+                         pin = int(input("Enter you PIN: "))
+                         if pin in pins:
+                              print('Welcome To Your Account')
+                              time.sleep(2)
+                              transactions()
+                              sys.exit()
+                         else:
+                              print('Incorrect PIN')
+                    except Exception as e:
+                         print('Use Numbers Only')
+                         break
+               break
+          except Exception as e:
+               print("Choose the correct pin")
+                    
+          
+#get_pin()
+
+# ref = logging.basicConfig(
+# filename='reference.log', level=logging.DEBUG, format='%(asctime)s:%(message)s')
+# logging.debug('Transaction Type: {}')
+
+
+get_pin()
